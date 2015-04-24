d8a0855c7a2720a15a5151b98cb6567d98c8646c
zhangliang <feidouyisheng_zl@163.com>
2015-04-24 09:15:44 +0800

    qq项目

---
+++ MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  QQDemo
+//
+//  Created by ZLMac on 15-4-21.
+//  Copyright (c) 2015年 lgwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UITabBarController
+
+@end
